6d92d6dda3260422ddfcf51409c187e83aae9b8b
Mehraz <mahmed6@ncsu.edu>
2022-07-26 22:57:35 -0400

    temp

--- src/main/java/io/jukelyn/github/FullStackDemo/FullStackDemoApplication.java
+++ src/main/java/io/jukelyn/github/FullStackDemo/FullStackDemoApplication.java
@@ -3,9 +3,18 @@ package io.jukelyn.github.FullStackDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Springboot app
+ *
+ * @author Mehraz Ahmed
+ */
 @SpringBootApplication
 public class FullStackDemoApplication {
-
+	/**
+	 * Starts the Springboot application
+	 *
+	 * @param args command line arguments (not used)
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(FullStackDemoApplication.class, args);
 	}
---
+++ src/main/java/io/jukelyn/github/FullStackDemo/model/Employee.java
@@ -0,0 +1,22 @@
+package io.jukelyn.github.FullStackDemo.model;
+
+/**
+ * Employee Class
+ *
+ * @author Mehraz Ahmed
+ */
+public class Employee {
+
+    /** Employee ID */
+    private Long id;
+
+    /** Employee name */
+    private String name;
+
+    /** Employee email */
+    private String email;
+
+    /** Job title */
+    private String jobTitle;
+
+}
